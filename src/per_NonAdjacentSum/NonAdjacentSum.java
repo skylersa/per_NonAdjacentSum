85085439e8dd4a220b31a315d119624eaf4bf003
Skyler Sauer <sjwsauer@gmail.com>
2021-11-20 02:42:12 -0800

    multiple buttons
    work on actual solve in obsidian
    involves sorting values then comparing the values to those around it, needs to account for numbers that are equal

--- src/per_NonAdjacentSum/NonAdjacentSum.java
+++ src/per_NonAdjacentSum/NonAdjacentSum.java
@@ -15,12 +15,14 @@ import javax.swing.SwingConstants;
 
 public class NonAdjacentSum extends JFrame implements ActionListener
 {
-//	private JTextField inputNum1, inputNum2, inputNum3;
+
 	private final int NUMBER_OF_NUMBERS = 7;
+
 	private JLabel[] numberLabel = new JLabel[NUMBER_OF_NUMBERS];
 	private JTextField numberInput[] = new JTextField[NUMBER_OF_NUMBERS];
-	int valueArray[] = new int[NUMBER_OF_NUMBERS];
-//	private Dimension labelDimension = new Dimension(5, 25);
+	private int valueArray[] = new int[NUMBER_OF_NUMBERS];
+
+	private int buttonPressed = 0;
 
 	public NonAdjacentSum()
 	{
@@ -28,44 +30,69 @@ public class NonAdjacentSum extends JFrame implements ActionListener
 
 		JPanel panel = new JPanel();
 
-		// rows - might want to add more for visualisation/ output of problem
-		// 0 columns because it is ignored anyway, should be 2
-		GridLayout panelLayout = new GridLayout(NUMBER_OF_NUMBERS + 1, 0);
+		// rows will be auto calculate based on how many it needs -
+		// # of components probably needs to be even
+		GridLayout panelLayout = new GridLayout(0, 2);
 		panel.setLayout(panelLayout);
 //		JLabel labelNum1 = new JLabel("#1");
 //		panel.add(labelNum1);
 
 		/*
-		 * creates the the necesary labels and input fields made possible by the
-		 * constant NUMBER_OF_NUMBERS current
+		 * creates the the necessary labels and input fields made
+		 * possible by the constant NUMBER_OF_NUMBERS current
 		 */
 
-		int currentDisplayNum = 0;
-
+		int currentDisplayNum = 1;
 		for (int i = 0; i < NUMBER_OF_NUMBERS; i++)
 		{
-			currentDisplayNum = i + 1;
+
 			numberLabel[i] = new JLabel("#" + currentDisplayNum);
 			numberLabel[i].setHorizontalAlignment(SwingConstants.RIGHT);
-//			numberLabel[i].setMaximumSize(labelDimension);
 			panel.add(numberLabel[i]);
 
 			numberInput[i] = new JTextField();
-			numberInput[i].setHorizontalAlignment(JTextField.RIGHT);
+			numberInput[i].setHorizontalAlignment(JTextField.LEFT);
 			panel.add(numberInput[i]);
 
+			currentDisplayNum++;
 		}
-		JButton buttonToCalculate = new JButton("Find highest non adjacent sum");
-		buttonToCalculate.addActionListener(this);
+//		ActionEvent solvePressed = new ActionEvent(this)
+		JButton buttonToCalculate = new JButton("solve");
+		buttonToCalculate.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				String tempParsingValue;
+				for (int i = 0; i < NUMBER_OF_NUMBERS; i++)
+				{
+					tempParsingValue = numberInput[i].getText();
+					valueArray[i] = Integer.parseInt(tempParsingValue);
+				}
+
+				{ // non block block for debug
+					System.out.println(valueArray[0]);
+					System.out.println(valueArray[1]);
+					System.out.println(valueArray[2]);
+					System.out.println(valueArray[3]);
+					System.out.println(valueArray[4]);
+					System.out.println(valueArray[5]);
+					System.out.println(valueArray[6]);
+				}
+
+			}
+		});
 		panel.add(buttonToCalculate);
 
-		JButton buttonToRandomize = new JButton("randomize values");
-		buttonToRandomize.addActionListener(this);
+		JButton buttonToRandomize = new JButton("rand");
+		buttonToRandomize.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				for (int i = 0; i < NUMBER_OF_NUMBERS; i++)
+					numberInput[i].setText(Integer.toString((int) (Math.random() * 20)));
+			}
+		});
 		panel.add(buttonToRandomize);
-
-//		inputNum1 = new JTextField(5);
-//		inputNum1.setHorizontalAlignment(JTextField.RIGHT);
-//		panel.add(inputNum1);
 
 		Container myContainer = getContentPane();
 		myContainer.add(panel, BorderLayout.CENTER);
@@ -73,28 +100,21 @@ public class NonAdjacentSum extends JFrame implements ActionListener
 //		myContainer.add(myContainer)
 	}
 
-//	public void createInputWithLabel(int labelNumber, )
-//	{
-//		
-//	}
+	public int solveSum(int[] values)
+	{
+		int maxNonAdjacentSum = 0;
+
+		return maxNonAdjacentSum;
+	}
 
 	public void actionPerformed(ActionEvent e)
 	{
-
-		for (int i = 0; i < NUMBER_OF_NUMBERS; i++)
-		{
-			String tempParsingValue;
-			System.out.println("l");
-			tempParsingValue = numberInput[i].getText();
-			valueArray[i] = Integer.parseInt(tempParsingValue);
-		}
-		System.out.println(valueArray);
 	}
 
 	public static void main(String[] args)
 	{
 		NonAdjacentSum window = new NonAdjacentSum();
-		window.setBounds(300, 300, 200, 180);
+		window.setBounds(300, 300, 100, 180);
 		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		window.setVisible(true);
 	}
